9af7f9fdea067019bf6d44593e35779b2e3185aa
perpich <mshpitonkov@yandex.ru>
2020-12-03 09:24:04 +0300

    Day 3 solution

---
+++ Day3.py
@@ -0,0 +1,20 @@
+from itertools import starmap
+from math import prod
+
+grid = [list(x.strip()) for x in open('Day3.txt').readlines()]
+
+
+def trajectory(a, b):
+    result = 0
+    i = j = 0
+
+    while i < len(grid):
+        if i < len(grid) and grid[i][j] == '#':
+            result += 1
+        j = (j + b) % len(grid[i])
+        i += a
+    return result
+
+
+print(prod(starmap(trajectory, [(1, 3)])))
+print(prod(starmap(trajectory, [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)])))
